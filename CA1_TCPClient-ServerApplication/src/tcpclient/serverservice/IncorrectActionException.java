a3b51f34ddb599486b18562f30eb525a14d02d5e
AaronKrns <73694811+AaronKrns@users.noreply.github.com>
2022-11-15 19:59:48 +0000

    Add files via upload

---
+++ CA1_TCPClient-ServerApplication/src/tcpclient/serverservice/IncorrectActionException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tcpclient.serverservice;
+
+/**
+ * IncorrectActionException.java
+ * Product Version: Apache NetBeans IDE 12.5
+ * @author Aaron Kearns (Student number: x20513033)
+ * Advanced Programming CA Part 2
+ * 05/11/2022
+*/
+public class IncorrectActionException extends Exception {
+    public String getMsg() {
+        return ("Invalid action");
+    }   
+}
