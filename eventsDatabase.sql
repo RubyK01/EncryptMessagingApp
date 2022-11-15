271ae1d1edad724bb7bed50bf442886ed87a194e
RubyK01 <73535878+RubyK01@users.noreply.github.com>
2022-11-15 22:23:42 +0000

    Database

---
+++ eventsDatabase.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS `events`;
+
+create database if not exists`events` default character set utf8 collate utf8_general_ci;
+
+use events;
+
+create table eventDetail(
+eventss varchar(255) not null
+);
